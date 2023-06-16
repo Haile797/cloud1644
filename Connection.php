59d9ae16546b6ab9cf0cd125a6c77b88195afe0d
Haile797 <Hailvgcc200225@fpt.edu.vn>
2023-06-16 15:44:34 +0700

    update

--- Connection.php
+++ Connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost', 'root','', 'online_shopping')
+$conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'z7e234cf9rwqz2m5','b27aenr7rv1wqfmz', 'cubzo85dfhqo99zo')
         or die("Can not connect database".mysqli_connect_error());
 ?>
